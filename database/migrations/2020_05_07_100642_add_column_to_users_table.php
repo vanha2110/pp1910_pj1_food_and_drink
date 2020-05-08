2c006312b7142ee3e0962a5f6fde4c4451040b8f
Minion <vanha2110@gmail.com>
2020-05-08 09:36:37 +0700

    create migration

---
+++ database/migrations/2020_05_07_100642_add_column_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar');
+            $table->string('phone');
+            $table->integer('role_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('phone');
+            $table->dropColumn('role_id');
+        });
+    }
+}
