e727fc1f9ed0b9b62d299fb9610f66914a6b91a8
Minion <vanha2110@gmail.com>
2020-05-19 13:09:32 +0700

    change password

---
+++ resources/views/web/user/password/index.blade.php
@@ -0,0 +1,41 @@
+@extends('web.layout.app')
+
+@section('content')
+<!--title-bar start-->
+@include('web.user.title_bar')
+<!--title-bar end-->	
+<!--my-account start-->
+<section class="my-account">			
+    <div class="profile-bg">
+        <img src="{{url('template_web/images/profile/bg-img.jpg')}}" alt="Responsive image">
+        <div class="my-Profile-dt">
+            <div class="container">
+                <div class="row">
+                    <div class="container">							
+                        <div class="profile-dpt">
+                            <img src="{{url('template_web/images/profile/dp-1.jpg')}}" alt="">
+                        </div>
+                        <div class="profile-all-dt">
+                            <div class="profile-name-dt">
+                                <h1>John Doe</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--my-account end-->	
+<!--my-account-tabs start-->
+<section class="all-profile-details">
+    <div class="container">
+        <div class="row">
+            @include('web.user.menu')
+            
+            @include('web.user.password.main')
+        </div>
+    </div>
+</section>
+<!--my-account-tabs end-->    
+@endsection
---
+++ resources/views/web/user/title_bar.blade.php
@@ -0,0 +1,19 @@
+<section class="title-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="left-title-text">
+                <h3>My Account</h3>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="right-title-text">  
+                    <ul>
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">My Account</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
