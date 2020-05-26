1248983146dc6a7aea0aa7651dfa0dbd3843ba3e
tuan anh <daotuananh201295@gmail.com>
2020-05-26 17:07:16 +0700

    trait

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,13 +2,14 @@
 
 namespace App\Services;
 
-use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use App\Repositories\Contracts\ProductInterface;
 use Illuminate\Support\Str;
+use App\Traits\ImageTrait;
 
 class ProductService
 {
+    use ImageTrait;
     public function __construct(ProductInterface $productRepository)
     {
         $this->productRepository = $productRepository;
@@ -16,30 +17,19 @@ class ProductService
 
     public function create(Request $request)
     {
-        $data = [
-            'category_id' => $request->get('category_id'),
-            'name' => $request->get('name'),
-            'slug' => Str::slug($request->get('name', '-')),
-            'image' => $request->get('image'),
-            'price' => $request->get('price'),
-            'description' => $request->get('description'),
-        ];
-
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->get('name', '-'));
+        $data['image'] =  $this->uploadImage($request, 'image');
+        
         $this->productRepository->create($data);
     }
 
     public function update(Request $request, $slug)
     {
-        $data = [
-            'category_id' => $request->get('category_id'),
-            'name' => $request->get('name'),
-            'slug' => Str::slug($request->get('name', '-')),
-            'image' => $request->get('image'),
-            'price' => $request->get('price'),
-            'description' => $request->get('description'),
-        ];
-        $product = $this->productRepository->updateBySlug($slug, $data);
-        $product_id = $this->productRepository->findBySlug($slug)->id;
-        ProductImage::whereProductId($product_id)->delete();
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->get('name', '-'));
+        $data['image'] =  $this->uploadImage($request, 'image');
+        
+        $this->productRepository->updateBySlug($slug, $data);
     }
 }
