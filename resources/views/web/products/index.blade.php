744d92fcff8bef0ac6d20f2b818128f10443cfee
vanha2110 <vanha2110@gmail.com>
2020-05-26 11:34:06 +0700

    feat(web):menu product

---
+++ resources/views/web/products/index.blade.php
@@ -0,0 +1,16 @@
+@extends('web.layout.app')
+@section('title', 'Products')
+@section('content')
+
+@include('web.products.title')
+
+<section class="all-partners">			
+    <div class="container">		
+        <div class="row">
+            @include('web.products.filter')	
+            @include('web.products.main')
+        </div>
+    </div>
+</section>
+
+@endsection
