578e2261647d61cf5b5c8c7a15c565f31d7c5559
vanha2110 <vanha2110@gmail.com>
2020-05-26 17:15:28 +0700

    feat(admin): Create trait

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,13 +2,14 @@
 
 namespace App\Services;
 
-use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use App\Repositories\Contracts\ProductInterface;
 use Illuminate\Support\Str;
+use App\Traits\ImageTrait;
 
 class ProductService
 {
+    use ImageTrait;
     public function __construct(ProductInterface $productRepository)
     {
         $this->productRepository = $productRepository;
@@ -16,43 +17,19 @@ class ProductService
 
     public function create(Request $request)
     {
-        $request->all();
         $data = $request->all();
         $data['slug'] = Str::slug($request->get('name', '-'));
-
-        if($request->hasFile('image')){
-            $file = $request->file('image')->getClientOriginalName();
-            $filename = pathinfo($file, PATHINFO_FILENAME);
-            $extension = $request->file('image')->getClientOriginalExtension();
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-            $request->file('image')->storeAs('public/img', $fileNameToStore);
-            $data['image'] = $fileNameToStore;
-            
-        }
+        $data['image'] =  $this->uploadImage($request, 'image');
         
         $this->productRepository->create($data);
     }
 
     public function update(Request $request, $slug)
     {
-        $request->all();
         $data = $request->all();
         $data['slug'] = Str::slug($request->get('name', '-'));
-
-        if($request->hasFile('image')){
-            $file = $request->file('image')->getClientOriginalName();
-            $filename = pathinfo($file, PATHINFO_FILENAME);
-            $extension = $request->file('image')->getClientOriginalExtension();
-            $fileNameToStore = $filename.'_'.time().'.'.$extension;
-            $request->file('image')->storeAs('public/img', $fileNameToStore);
-            $data['image'] = $fileNameToStore;
-            
-        }
+        $data['image'] =  $this->uploadImage($request, 'image');
         
-        $this->productRepository->create($data);
-
-        $product = $this->productRepository->updateBySlug($slug, $data);
-        $product_id = $this->productRepository->findBySlug($slug)->id;
-        ProductImage::whereProductId($product_id)->delete();
+        $this->productRepository->updateBySlug($slug, $data);
     }
 }
